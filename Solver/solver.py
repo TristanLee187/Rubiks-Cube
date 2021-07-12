7ae122bc1599f9456eb6228ba3dee745eee740a8
Tristan Lee <tlee10@stuy.edu>
2021-07-11 22:46:06 -0400

    complete solver side of interaction

--- Solver/solver.py
+++ Solver/solver.py
@@ -4,5 +4,18 @@ from FastCube import FastCube
 from FullSolve import full_solve
 from sys import argv
 
-if __name__ == '__main__':
+
+def scramble_num_to_str(scramble):
+    ans = ''
+    moves = ['U', 'F', 'R', 'B', 'L', 'D']
+    for num in scramble:
+        add = moves[num // 3]
+        add += ['', '\'', '2'][num % 3]
+        ans += add + ' '
+    return ans
+
+
+def solver():
     cube = FastCube(argv[1])
+    ans = full_solve(cube)
+    return scramble_num_to_str(ans)
