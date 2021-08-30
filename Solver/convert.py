eb267b7754ddb5cd69bcb0f94f380d96f1c4e6cc
TristanLee <tlee10@stuy.edu>
2021-08-29 22:28:53 -0400

    edit convert procedure to support different string labels for each of the faces, such as moving the centers around with M moves. solver should now be able to solve any scramble possible except illegal cases inputted with 'see' in the terminal script

--- Solver/convert.py
+++ Solver/convert.py
@@ -4,17 +4,6 @@
 PIECE_LAYOUT = [12, 0, 13, 4, -1, 5, 15, 1, 14, 12, 4, 15, 15, 1, 14, 14, 5, 13, 13, 0, 12, 11, -1, 10, 10, -1, 9, 9,
                 -1, 8, 8, -1, 11, 16, 7, 19, 19, 2, 18, 18, 6, 17, 17, 3, 16, 19, 2, 18, 7, -1, 6, 16, 3, 17]
 
-PIECES_NUMS_TO_COLORS = [('B', 'W'), ('G', 'W'), ('G', 'Y'), ('B', 'Y'), ('O', 'W'), ('R', 'W'), ('R', 'Y'), ('O', 'Y'),
-                         ('B', 'R'), ('G', 'R'), ('G', 'O'), ('B', 'O'), ('B', 'O', 'W'), ('B', 'R', 'W'),
-                         ('G', 'R', 'W'), ('G', 'O', 'W'), ('B', 'O', 'Y'), ('B', 'R', 'Y'), ('G', 'R', 'Y'),
-                         ('G', 'O', 'Y')]
-
-PIECES_COLORS_TO_NUMS = {('B', 'W'): 0, ('G', 'W'): 1, ('G', 'Y'): 2, ('B', 'Y'): 3, ('O', 'W'): 4, ('R', 'W'): 5,
-                         ('R', 'Y'): 6, ('O', 'Y'): 7, ('B', 'R'): 8, ('G', 'R'): 9, ('G', 'O'): 10, ('B', 'O'): 11,
-                         ('B', 'O', 'W'): 12, ('B', 'R', 'W'): 13, ('G', 'R', 'W'): 14, ('G', 'O', 'W'): 15,
-                         ('B', 'O', 'Y'): 16, ('B', 'R', 'Y'): 17, ('G', 'R', 'Y'): 18, ('G', 'O', 'Y'): 19}
-
-
 FACES = [
     list(range(9)),
     [12, 13, 14, 24, 25, 26, 36, 37, 38],
@@ -25,9 +14,9 @@ FACES = [
 ]
 
 
-def def_piece(piece):
+def def_piece(piece, colors_to_nums, top_color, left_color, front_color, right_color, back_color, bottom_color):
     ans = [-1, -1]
-    ans[0] = PIECES_COLORS_TO_NUMS[tuple(sorted([i[0] for i in piece]))]
+    ans[0] = colors_to_nums[tuple(sorted([i[0] for i in piece]))]
     stickers = {}
     n = len(piece)
     for i in range(6):
@@ -37,22 +26,24 @@ def def_piece(piece):
     if n == 2:  # edge
         if 1 in stickers or 3 in stickers:
             look = 1 if 1 in stickers else 3
-            if stickers[look] in 'RO':
+            if stickers[look] in [right_color, left_color]:
                 ans[1] = 1
-            elif stickers[look] in 'WY' and ('G' in stickers.values() or 'B' in stickers.values()):
+            elif stickers[look] in [top_color, bottom_color] and \
+                    (front_color in stickers.values() or back_color in stickers.values()):
                 ans[1] = 1
             else:
                 ans[1] = 0
         else:
             look = 2 if 2 in stickers else 4
-            if stickers[look] in 'GB':
+            if stickers[look] in [front_color, back_color]:
                 ans[1] = 1
-            elif stickers[look] in 'WY' and ('R' in stickers.values() or 'O' in stickers.values()):
+            elif stickers[look] in [top_color, bottom_color] and \
+                    (right_color in stickers.values() or left_color in stickers.values()):
                 ans[1] = 1
             else:
                 ans[1] = 0
     else:
-        check = 'R' if 'R' in stickers.values() else 'O'
+        check = right_color if right_color in stickers.values() else left_color
         axis = [i for i in stickers if stickers[i] == check][0]
         if axis in [2, 4]:
             ans[1] = 0
@@ -65,6 +56,23 @@ def def_piece(piece):
 
 def convert(layout, cube):
     stickers = layout.split()
+    top_color = stickers[4]
+    left_color = stickers[22]
+    front_color = stickers[25]
+    right_color = stickers[28]
+    back_color = stickers[31]
+    bottom_color = stickers[49]
+    pieces_colors_to_nums = {(back_color, top_color): 0, (front_color, top_color): 1, (front_color, bottom_color): 2,
+                             (back_color, bottom_color): 3, (left_color, top_color): 4, (right_color, top_color): 5,
+                             (right_color, bottom_color): 6, (left_color, bottom_color): 7,
+                             (back_color, right_color): 8, (front_color, right_color): 9, (front_color, left_color): 10,
+                             (back_color, left_color): 11, (back_color, left_color, top_color): 12,
+                             (back_color, right_color, top_color): 13, (front_color, right_color, top_color): 14,
+                             (front_color, left_color, top_color): 15, (back_color, left_color, bottom_color): 16,
+                             (back_color, right_color, bottom_color): 17, (front_color, right_color, bottom_color): 18,
+                             (front_color, left_color, bottom_color): 19}
+    pieces_colors_to_nums = {tuple(sorted([i[0] for i in piece])):pieces_colors_to_nums[piece] for piece in
+                             pieces_colors_to_nums}
     z = list(zip(PIECE_LAYOUT, stickers))
     pieces = [[] for _ in range(20)]
     for i in range(54):
@@ -72,6 +80,7 @@ def convert(layout, cube):
         if entry[0] != -1:
             pieces[entry[0]] += [(entry[1], i)]
     for i in range(20):
-        ans = def_piece(pieces[i])
+        ans = def_piece(pieces[i], pieces_colors_to_nums, top_color, left_color, front_color, right_color, back_color,
+                        bottom_color)
         cube.ps[i] = ans[0]
         cube.ops[i] = ans[1]
